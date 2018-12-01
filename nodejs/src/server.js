e8758da96510c632df3f2f07c02803e0792a55a8
Vlad ONCIOIU <voncioiu@bitdefender.com>
2018-12-01 20:41:19 +0200

    Fix.

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 7000;
 
-app.get('/api', (req, res) => res.sendStatus(405));
+// app.get('/api', (req, res) => res.sendStatus(405));
+app.get('/api', (req, res) => res.send("Hello from Node!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
