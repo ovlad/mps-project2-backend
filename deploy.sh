a96bd54e5ee754e884bbbd1d73c5ebf55323dbe9
Vlad ONCIOIU <voncioiu@bitdefender.com>
2018-12-02 12:21:44 +0200

    Fix kubernetes deploy script.

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ git pull
 
 export PROJECT_ID="$(gcloud config get-value project -q)"
 
-docker build -t gcr.io/${PROJECT_ID}/nginx:latest ./nginx/ -f ./nginx/Dockerfile.live
-docker build -t gcr.io/${PROJECT_ID}/nodejs:latest ./nodejs/ -f ./nodejs/Dockerfile
-docker build -t gcr.io/${PROJECT_ID}/python:latest ./python/ -f ./python/Dockerfile
+docker build -t gcr.io/${PROJECT_ID}/nginx:latest ./mps-project2-backend/nginx/ -f ./mps-project2-backend/nginx/Dockerfile.live
+docker build -t gcr.io/${PROJECT_ID}/nodejs:latest ./mps-project2-backend/nodejs/ -f ./mps-project2-backend/nodejs/Dockerfile
+docker build -t gcr.io/${PROJECT_ID}/python:latest ./mps-project2-backend/python/ -f ./mps-project2-backend/python/Dockerfile
 
 docker push gcr.io/${PROJECT_ID}/nginx:latest
 docker push gcr.io/${PROJECT_ID}/nodejs:latest
 docker push gcr.io/${PROJECT_ID}/python:latest
 
-kubectl apply -f ./nginx/mps-backend-nginx-deployment.yaml
-kubectl apply -f ./nodejs/mps-backend-nodejs-deployment.yaml
-kubectl apply -f ./python/mps-backend-python-deployment.yaml
+kubectl apply -f ./mps-project2-backend/nginx/mps-backend-nginx-deployment.yaml
+kubectl apply -f ./mps-project2-backend/nodejs/mps-backend-nodejs-deployment.yaml
+kubectl apply -f ./mps-project2-backend/python/mps-backend-python-deployment.yaml
 
 kubectl patch deployment mps-backend-nginx -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
 kubectl patch deployment mps-backend-nodejs -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
