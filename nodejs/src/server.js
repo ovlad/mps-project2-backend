f46de382243059fa0c3b3317b3e50a45faf4d75e
Vlad ONCIOIU <voncioiu@bitdefender.com>
2018-12-06 13:03:12 +0200

    Add authorization flow.

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,10 +1,98 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+// load env file
+require('dotenv').config();
+
 const app = express();
-const port = 7000;
+const PORT = 7000;
 
-// app.get('/api', (req, res) => res.sendStatus(405));
-app.get('/api', (req, res) => res.send("Hello World! This is a greeting from the NodeJS API!"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '10mb'}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// request middleware token verification
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if (_.isUndefined(bearerHeader)) {
+        res.sendStatus(401);
+    } else {
+        const token = bearerHeader.split(' ')[1];
+
+        jwt.verify(token, process.env.JWT_SECRET, error => {
+            if (error) {
+                res.sendStatus(401);
+                console.log(error);
+            } else {
+                next();
+            }
+        });
+    }
+}
+
+// app.get('/', (req, res) => res.sendStatus(405));
+// app.get('/', (req, res) => res.send("Hello World! This is a greeting from the NodeJS API!"));
+
+// POST /login
+app.post('/login', (req, res) => {
+    const body = req.body || {};
+
+    // get params from body
+    const email = body.email;
+    const password = body.password;
+
+    // take username information from db
+    // ...
+    const user = {
+        email,
+        password
+    };
+
+    jwt.sign({user}, process.env.JWT_SECRET, (error, token) => {
+        if (error) {
+            res.sendStatus(401);
+            console.log(error);
+        } else {
+            res.json({
+                token
+            });
+        }
+    });
+});
+
+// POST /refresh
+app.post('/refresh', (req, res) => {
+    const body = req.body || {};
+
+    // get params from body
+    const token = body.token;
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, data) => {
+        if (error) {
+            res.sendStatus(401);
+            console.log(error);
+        } else {
+            jwt.sign({user: data.user}, process.env.JWT_SECRET, (error, token) => {
+                if (error) {
+                    res.sendStatus(401);
+                    console.log(error);
+                } else {
+                    res.json({
+                        token
+                    });
+                }
+            });
+        }
+    });
+});
+
+// POST /bla
+app.post('/bla', verifyToken, (req, res) => {
+    res.send("Bla from the NodeJS API!");
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
--- python/src/server.py
+++ python/src/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/api")
+@app.route("/")
 def hello():
     return "Hello World! This is a greeting from the Python API!"
 
