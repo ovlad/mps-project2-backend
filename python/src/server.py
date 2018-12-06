4cf533412fff789ce76938256179f153854ccb36
Andrei Costin Lica <andreicostinlica@gmail.com>
2018-12-07 01:57:50 +0200

    python initial work

--- python/src/server.py
+++ python/src/server.py
@@ -1,9 +1,13 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+import models
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mps2admin:ParolaSecure1!@mps2db.mysql.database.azure.com/mps2projectdev2'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
+    db.create_all()
     return "Hello World! This is a greeting from the Python API!"
 
 if __name__ == "__main__":
