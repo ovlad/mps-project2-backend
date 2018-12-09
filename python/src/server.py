89c183d588011790e0d7f404b871e79637cd637d
Andrei Costin Lica <andreicostinlica@gmail.com>
2018-12-09 18:13:11 +0200

    change directory structure to get around issues; implement marshmallow_enum to enable Enum type serialization. create working demo API for donnor

--- python/src/server.py
+++ python/src/server.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from database import init_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from config import dev as config
+
+db_host = config.DATABASE_CONFIG['host']
+db_name = config.DATABASE_CONFIG['dbname']
+db_user = config.DATABASE_CONFIG['user']
+db_password = config.DATABASE_CONFIG['password']
+db_port = config.DATABASE_CONFIG['port']
+db_ssl_ca = config.DATABASE_CONFIG['ssl_ca']
+
+
+db_connect_string = "mysql://" + db_user + ":" + db_password + "@" + db_host + "/" + db_name + "?ssl_ca=" + db_ssl_ca
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_connect_string
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-@app.route("/")
-def hello():
-    tables = init_db()
-    output = ""
-    for entry in tables:
-        output += entry + " "
-    return output
+
 if __name__ == "__main__":
+    from views import *
     app.run(debug=True,host='0.0.0.0',port=7000)
---
+++ python/src/views.py
@@ -0,0 +1,28 @@
+from server import app
+from flask import request, jsonify
+from models import Doctor, Donation, Donor, Employee, Hospital, Request, TransfusionCenter
+from schemas import DoctorSchema, DonationSchema, DonorSchema, EmployeeSchema, HospitalSchema, RequestSchema, TransfusionCenterSchema
+
+@app.route("/")
+def hello():
+    tables = ""
+    result = db.session.execute("show tables;").fetchall()
+    for entry in result:
+        tables += str(entry.items()[0][1]) + " "
+    return tables
+
+@app.route("/employee", methods=['GET'])
+def employee_api():
+    if request.method == "GET":
+        return " ".join(Employee.query.all())
+
+@app.route("/donor", methods=['GET'])
+def donor_api():
+    if request.method == "GET":
+        donors = Donor.query.all()
+        donor_schema = DonorSchema(many=True)
+        output = donor_schema.dump(donors).data
+        return jsonify(output)
+
+
+
